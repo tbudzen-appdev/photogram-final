af6380e10d0f6e84d03492ec1b115dd8c3aa18e5
budzen <tbudzen@uchicago.edu>
2020-02-24 20:46:04 +0000

    finished sign up and added stuff in app controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,15 @@
 class CommentsController < ApplicationController
 
+  def create
+    comment = Comment.new
+
+    comment.author_id = session.fetch(:user_id)
+    comment.photo_id = params.fetch("input_photo_id")
+    comment.body = params.fetch("input_body")
+
+    comment.save
+
+    redirect_to("/photos/#{comment.photo_id}")
+  end
 
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,44 @@
 class PhotosController < ApplicationController
 
 def index
-render(:template => "/photos/photo_index.html.erb")
-end
+    @photos = Photo.all
+    render({ :template => "photos/photo_index.html.erb"})
+  end
 
+  def create
+    user_id = session.fetch(:user_id)
+    image = params.fetch("input_image")
+    caption = params.fetch("input_caption")
+    photo = Photo.new
+    photo.owner_id = user_id
+    photo.image = image
+    photo.caption = caption
+    photo.save
+    redirect_to("/photos/#{photo.id}")
+  end
+
+  def show
+    p_id = params.fetch("the_photo_id")
+    @photo = Photo.where({:id => p_id }).first
+    render({:template => "photos/details.html.erb"})
+  end
+
+  def destroy
+    id = params.fetch("the_photo_id")
+    photo = Photo.where({ :id => id }).at(0)
+    photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def update
+    id = params.fetch("the_photo_id")
+    photo = Photo.where({ :id => id }).at(0)
+    photo.caption = params.fetch("input_caption")
+    photo.image = params.fetch("input_image")
+    photo.save
+
+    redirect_to("/photos/#{photo.id}")
+  end
+  
 end
